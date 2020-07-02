7abf99c129305e9dd3e12ad9d84a0b007895cce3
Matthew Lister <mdlister24@hotmail.com>
2020-07-02 20:05:48 +0000

    Removed some files, too much data broke the system so restarting bits

--- help/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    """ View to return index page """
-    return render(request, 'help/helpthecause.html')
