2ead0e3cd0389764289a89518913000b302f32b6
Matthew Lister <mdlister24@hotmail.com>
2020-07-09 20:23:37 +0000

    Broken and fixed trip issue

--- trips/views.py
+++ trips/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
-from .models import Trips
 
 # Create your views here.
-
-
-def all_trips(request):
-    """ View to display trips with search options"""
-
-    trips = Trips.objects.all()
-
-    context = {
-        'trips': trips,
-    }
-    return render(request, 'trips/trips.html', context)
